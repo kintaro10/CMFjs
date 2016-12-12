cbc3d99af6344f68e824eea35756845b80135cf4
Cams03 <camille.francia@hotmail.fr>
2016-12-12 14:38:37 +0100

    test code added

--- app_obstacles.js
+++ app_obstacles.js
@@ -1,10 +1,55 @@
-var asteroide = document.getElementByID("image1");
+$(document).keydown(function(e){
 
-var max = 7;
+  if (e.which == 39) {// Vers la droite
+  posX = parseInt($('#soucoupe').css('left'));
+  if (posX < 470)
+    $('#soucoupe').css('left', posX+30);
+  }
 
-var nb = Math.floor(max * Math.random());
+  if (e.which == 37) { // Vers la gauche
+	posX = parseInt($('#soucoupe').css('left'));
+  if (posX > 20)
+  	$('#soucoupe').css('left', posX-30);
+  }
 
-document.write
-	("image1");
+  if (e.which == 40) { // Vers le bas
+  posY = parseInt($('#soucoupe').css('top'));
+  if (posY < 230)
+    $('#soucoupe').css('top', posY+30);
+	}
 
+  if (e.which == 38) { // Vers le haut
+  posY = parseInt($('#soucoupe').css('top'));
+  if (posY > 20)
+    $('#soucoupe').css('top', posY-30);
+	}
 
+  if (e.which == 36) {// Vers le haut et la gauche
+  posX = parseInt($('#soucoupe').css('left'));
+  posY = parseInt($('#soucoupe').css('top'));
+  if ((posY > 20) && (posX > 20))
+    $('#soucoupe').css('left', posX-30).css('top', posY-30);
+  }
+
+  if (e.which == 33) { // Vers le haut et la droite
+  posX = parseInt($('#soucoupe').css('left'));
+  posY = parseInt($('#soucoupe').css('top'));
+  if ((posY > 20) && (posX < 470))
+    $('#soucoupe').css('left', posX+30).css('top', posY-30);
+  }
+
+  if (e.which == 35) { // Vers le bas et la gauche
+  posX = parseInt($('#soucoupe').css('left'));
+  posY = parseInt($('#soucoupe').css('top'));
+  if ((posX > 20) && (posY < 230))
+    $('#soucoupe').css('left', posX-30).css('top', posY+30);
+	}
+
+  if (e.which == 34) { // Vers le bas et la droite
+  posX = parseInt($('#soucoupe').css('left'));
+  posY = parseInt($('#soucoupe').css('top'));
+  if ((posY < 230) && (posX < 470))
+    $('#soucoupe').css('left', posX+30).css('top', posY+30);
+  }
+
+});
