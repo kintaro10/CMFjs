7f4dd5aad691ac505f30e3d9b7edaa106ba6b634
Cams03 <camille.francia@hotmail.fr>
2016-12-13 16:33:32 +0100

    jquery removed, new images rockets removed, js updated

--- app_obstacles.js
+++ app_obstacles.js
@@ -1,76 +1,44 @@
-function afficheElements() {
+function spawnPlanet() {
 
   const margin = 20;
   // VISIBILITE BON / MAUVAIS
   const elemType = Math.floor(Math.random()*2); //tirage aléatoire d'un nombre entre 0 et 2
-  let enabled, disabled;
+
+  let newPlanet = document.createElement('div').addClass("fly");
 
   if(elemType === 0) {
-    enabled = $("#bon"); // si l'élément tiré est égal à 0 la var enabled = bon
-    disabled = $("#mauvais")
+    newPlanet = url ("solar_system.png");
   } else {
-    enabled = $("#mauvais"); // si l'élément tiré n'est pas égal à 0 la var enabled = mauvais
-    disabled = $("#bon");
+    newPlanet = url("saturn.png");
   }
-  disabled.hide(); //disabled est cachée
-  enabled.show();// enabled est montrée
+
+div.appendchild(newPlanet);
 
   // DEFINITION DES DIRECTIONS
   // 0:top, 1:right, 2:bottom, 3:left
   const side = Math.floor(Math.random() * 4);// tirage aléatoire d'un nombre entre 0 et 4 qui définira le point de départ
-  const fromX = Math.floor(Math.random()*(window.innerWidth - 2*margin))+margin;//tirage aléatoire d'un nombre entre 20 (margin) et 980 (largeur de la fenêtre - margin) afin de définir une position horizontale
+  const fromX = Math.floor(Math.random()* (window.innerWidth - 2*margin))+margin;//tirage aléatoire d'un nombre entre 20 (margin) et 980 (largeur de la fenêtre - margin) afin de définir une position horizontale
   const fromY = Math.floor(Math.random()*(window.innerHeight - 2*margin))+margin;
 
   if(side === 0) { // top
-    enabled.css("top", "0").css("left",fromX);
-    enabled.css("animation-name", "fly-from-top");
-    enabled.show();
+    newPlanet.css("top", "0").css("left",fromX);
+    newPlanet.css("animation-name", "fly-from-top");
+    newPlanet.show();
   } else if (side === 1) { // right
-    enabled.css("right", "0").css("top",fromY);
-    enabled.css("animation-name", "fly-from-right");
-    enabled.show();
+    newPlanet.css("right", "0").css("top",fromY);
+    newPlanet.css("animation-name", "fly-from-right");
+    newPlanet.show();
   } else if (side === 2) { // bottom
-    enabled.css("bottom", "0").css("left",fromX);
-    enabled.css("animation-name", "fly-from-bottom");
-    enabled.show();
+    newPlanet.css("bottom", "0").css("left",fromX);
+    newPlanet.css("animation-name", "fly-from-bottom");
+    newPlanet.show();
   } else if (side === 3) { // left
-    enabled.css("left", "0").css("top",fromY);
-    enabled.css("animation-name", "fly-from-left");
-    enabled.show();
+    newPlanet.css("left", "0").css("top",fromY);
+    newPlanet.css("animation-name", "fly-from-left");
+    newPlanet.show();
   }
 
 
-
-/*  $("img").click(function(){
-
-    var fly = $(this).attr("fly");
-
-    $("img").load(fly);
-    $(this).addClass("fly");
-
-});
-
-$(document).addEventListener("click", function() {
-  $("img").attr("class", "fly");
-});*/
-
-// $(function() {
-//  $("#jeu").click(function() {
-//    $("normal").toggleClass("fly");
-//  });
-// });
-
-  // if (elemType == 0) {
-  //
-	// 	$("#bon").css("top", "5").css("left",fromX);
-  //   $("#bon").show();
-  //   $("#mauvais").hide();
-	// 	}
-	// 	else {
-  //   $("#mauvais").css("top","5").css("left",fromX);
-  //   $("#mauvais").show();
-  //   $("#bon").hide();
-	// 	}
 };
 
-setInterval(afficheElements, 1000);
+setInterval(spawnPlanet, 2000);
