ec8d810f22b83c9601f9a483b248cff94a2bfd3b
Cams03 <camille.francia@hotmail.fr>
2016-12-13 14:48:01 +0100

    html updated, css:animation top  added, js: random element ok, random side added

--- app_obstacles.js
+++ app_obstacles.js
@@ -1,9 +1,34 @@
 function afficheElements() {
 
-  var fromX = Math.floor(Math.random()*500)+20;
-  var fromY = Math.floor(Math.random()*3)+5;
+  const margin = 20;
+  // visibility bon/mauvais
+  const elemType = Math.floor(Math.random()*2);
+  let enabled, disabled;
 
-  var elemType = Math.floor(Math.random()*2);
+  if(elemType === 0) {
+    enabled = $("#bon");
+    disabled = $("#mauvais")
+  } else {
+    enabled = $("#mauvais");
+    disabled = $("#bon");
+  }
+  disabled.hide();
+  enabled.show();
+  // 0 top 1 right 2 bottom 3 left
+  const side = Math.floor(Math.random() * 4);
+  const fromX = Math.floor(Math.random()*(window.innerWidth - 2*margin))+margin;
+
+  if(side === 0) { // top
+    enabled.css("top", "0").css("left",fromX);
+    enabled.css("animation-name", "fly-from-top");
+    enabled.show();
+  } else if (side === 2) { // bottom
+    enabled.css("bottom", "0").css("left",fromX);
+    enabled.css("animation-name", "fly-from-bottom");
+    enabled.show();
+  }
+
+
 
 /*  $("img").click(function(){
 
@@ -18,23 +43,23 @@ $(document).addEventListener("click", function() {
   $("img").attr("class", "fly");
 });*/
 
-$(function() {
- $("#jeu").click(function() {
- $("normal").toggleClass("fly");
- });
-});
+// $(function() {
+//  $("#jeu").click(function() {
+//    $("normal").toggleClass("fly");
+//  });
+// });
 
-  if (elemType == 0) {
-
-		$("#bon").css("top", "5").css("left",fromX);
-    $("#bon").show();
-    $("#mauvais").css("display","none");
-		}
-		else {
-    $("#mauvais").css("top","5").css("left",fromX);
-    $("#mauvais").show();
-    $("#bon").css("display","none");
-		}
+  // if (elemType == 0) {
+  //
+	// 	$("#bon").css("top", "5").css("left",fromX);
+  //   $("#bon").show();
+  //   $("#mauvais").hide();
+	// 	}
+	// 	else {
+  //   $("#mauvais").css("top","5").css("left",fromX);
+  //   $("#mauvais").show();
+  //   $("#bon").hide();
+	// 	}
 };
 
 setInterval(afficheElements, 1000);
