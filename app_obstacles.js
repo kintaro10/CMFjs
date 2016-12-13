2d0965ce4a1a3cbe589c6da9f27395c7f8091560
Cams03 <camille.francia@hotmail.fr>
2016-12-13 12:04:33 +0100

    test transform onclick

--- app_obstacles.js
+++ app_obstacles.js
@@ -2,31 +2,39 @@ function afficheElements() {
 
   var fromX = Math.floor(Math.random()*500)+20;
   var fromY = Math.floor(Math.random()*3)+5;
-	var toX = Math.floor(Math.random()*500)+20;
-	var toY = Math.floor(Math.random()*300)+20;
 
   var elemType = Math.floor(Math.random()*2);
 
-  if (elemType == 0) {
-		var depart = Math.floor(Math.random()*2);
+/*  $("img").click(function(){
 
-		if (depart == 0) {
-    $('#bon').css('top', "5").css('left',fromX);
-    $('#bon').show();
-    $('#mauvais').css('display','none');
+    var fly = $(this).attr("fly");
+
+    $("img").load(fly);
+    $(this).addClass("fly");
+
+});
+
+$(document).addEventListener("click", function() {
+  $("img").attr("class", "fly");
+});*/
+
+$(function() {
+ $("#jeu").click(function() {
+ $("normal").toggleClass("fly");
+ });
+});
+
+  if (elemType == 0) {
+
+		$("#bon").css("top", "5").css("left",fromX);
+    $("#bon").show();
+    $("#mauvais").css("display","none");
 		}
 		else {
-    $('#bon').css('bottom', "5").css('left',fromX);
-    $('#bon').show();
-    $('#mauvais').css('display','none');
+    $("#mauvais").css("top","5").css("left",fromX);
+    $("#mauvais").show();
+    $("#bon").css("display","none");
 		}
-  }
-  else {
-    $('#mauvais').css('top',"5").css('left',fromX);
-    $('#mauvais').show();
-    $('#bon').css('display','none');
-  }
-
 };
 
 setInterval(afficheElements, 1000);
