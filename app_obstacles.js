14f9f966f87cb39205007a1f26fe85f49bffef53
Cams03 <camille.francia@hotmail.fr>
2016-12-14 14:52:31 +0100

    commit sur lequel revenir si jamais je foire la suite

--- app_obstacles.js
+++ app_obstacles.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
 
-let start = prompt("Entrez votre prénom :");
-alert("Bonjour " + start + "," + " on y va ?");
+  let start = prompt("Entrez votre prénom :");
+  alert("Bonjour " + start + "," + " on y va ?");
 
 });
 
@@ -9,61 +9,57 @@ function spawnPlanet() {
 
   const margin = 20;
   // VISIBILITE BON / MAUVAIS
-  const elemType = Math.floor(Math.random()*2); //tirage aléatoire d'un nombre entre 0 et 2
-  let newPlanet = document.createElement("img");
-  newPlanet.className += "fly";
+  const elemType = Math.floor(Math.random()*2); //tirage aléatoire d'un nombre entre 0 et 2 (car 2 éléments 1 bon et 1 mauvais)
+  console.log(elemType);
+  let newPlanet = document.createElement("img");// création d'un élément image
+  newPlanet.className += "fly";// ajout de la classe fly à l'élément créé
 
   if(elemType === 0) {
-    newPlanet.src = "solar-system.png";
+    newPlanet.src = "solar-system.png";// élément à toucher pour gagner 5 pts
   } else {
-    newPlanet.src = "saturn.png";
+    newPlanet.src = "saturn.png"; // élément à éviter
   }
 
-  document.getElementById('jeu').appendChild(newPlanet);
+
+  document.getElementById('jeu').appendChild(newPlanet);//rattache l'objet créé à son parent (#jeu)
 
   // DEFINITION DES DIRECTIONS
   // 0:top, 1:right, 2:bottom, 3:left
   const side = Math.floor(Math.random() * 4);// tirage aléatoire d'un nombre entre 0 et 4 qui définira le point de départ
   const fromX = Math.floor(Math.random()* (window.innerWidth - 2*margin))+margin;//tirage aléatoire d'un nombre entre 20 (margin) et 980 (largeur de la fenêtre - margin) afin de définir une position horizontale
-  const fromY = Math.floor(Math.random()*(window.innerHeight - 2*margin))+margin;
-  console.log(fromX);
+  const fromY = Math.floor(Math.random()*(window.innerHeight - 2*margin))+margin;//tirage aléatoire d'un nombre entre 20 (margin) et 980 (largeur de la fenêtre - margin) afin de définir une position verticale
 
   if(side === 0) { // top
     newPlanet.style.top = 0;
     newPlanet.style.left = fromX;
     newPlanet.style.animationName = "fly-from-top";
 
-    console.log('side0 : '+newPlanet);
   } else if (side === 1) { // right
     newPlanet.style.right = 0;
     newPlanet.style.top = fromY;
     newPlanet.style.animationName = "fly-from-right";
 
-    console.log('side1 : '+newPlanet);
   } else if (side === 2) { // bottom
     newPlanet.style.bottom = 0;
     newPlanet.style.left = fromX;
     newPlanet.style.animationName = "fly-from-bottom";
 
-    console.log('side2 : '+newPlanet);
   } else if (side === 3) { // left
     newPlanet.style.left = 0;
     newPlanet.style.top = fromY;
     newPlanet.style.animationName = "fly-from-left";
-
-    console.log('side3 : '+newPlanet);
   }
 
 };
 
 setInterval(spawnPlanet, 2000);
 
-var score = 0;
-alert("Votre score est de");
+/*var score = 0;
+alert("Votre score est de :" )
 
 switch (score) {
 case score < 50 :
-    alert("T'es bidon");
+     alert("T'es bidon");
     break;
 case score > 51 && score < 100:
     alert("Pas mal mais peut mieux faire");
@@ -73,4 +69,4 @@ case score > 100:
     break;
 default:
     alert("Je ne comprends pas abruti!");
-}
+};*/
