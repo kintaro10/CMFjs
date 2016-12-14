0188db617daa499faf3307da7b411d63e1da3093
Cams03 <camille.francia@hotmail.fr>
2016-12-14 10:16:20 +0100

    html: img removed, css:fly updated, js: newPlanet updated+ if else updated

--- app_obstacles.js
+++ app_obstacles.js
@@ -4,39 +4,50 @@ function spawnPlanet() {
   // VISIBILITE BON / MAUVAIS
   const elemType = Math.floor(Math.random()*2); //tirage aléatoire d'un nombre entre 0 et 2
 
-  let newPlanet = document.createElement('div').addClass("fly");
+  let newPlanet = document.createElement("img");
+  newPlanet.className += "fly"
 
   if(elemType === 0) {
-    newPlanet = url ("solar_system.png");
+    newPlanet.src = "solar-system.png";
   } else {
-    newPlanet = url("saturn.png");
+    newPlanet.src = "saturn.png";
   }
 
-div.appendchild(newPlanet);
+  document.getElementById('jeu').appendChild(newPlanet);
 
   // DEFINITION DES DIRECTIONS
   // 0:top, 1:right, 2:bottom, 3:left
   const side = Math.floor(Math.random() * 4);// tirage aléatoire d'un nombre entre 0 et 4 qui définira le point de départ
   const fromX = Math.floor(Math.random()* (window.innerWidth - 2*margin))+margin;//tirage aléatoire d'un nombre entre 20 (margin) et 980 (largeur de la fenêtre - margin) afin de définir une position horizontale
   const fromY = Math.floor(Math.random()*(window.innerHeight - 2*margin))+margin;
+  console.log(side);
 
   if(side === 0) { // top
-    newPlanet.css("top", "0").css("left",fromX);
-    newPlanet.css("animation-name", "fly-from-top");
-    newPlanet.show();
+    newPlanet.style.top = 0;
+    newPlanet.style.left = fromX;
+    newPlanet.style.animationName = "fly-from-top";
+
+    console.log('side0 : '+newPlanet);
   } else if (side === 1) { // right
-    newPlanet.css("right", "0").css("top",fromY);
-    newPlanet.css("animation-name", "fly-from-right");
-    newPlanet.show();
+    newPlanet.style.right = 0;
+    newPlanet.style.top = fromY;
+    newPlanet.style.animationName = "fly-from-right";
+
+    console.log('side1 : '+newPlanet);
   } else if (side === 2) { // bottom
-    newPlanet.css("bottom", "0").css("left",fromX);
-    newPlanet.css("animation-name", "fly-from-bottom");
-    newPlanet.show();
+    newPlanet.style.bottom = 0;
+    newPlanet.style.left = fromX;
+    newPlanet.style.animationName = "fly-from-bottom";
+
+    console.log('side2 : '+newPlanet);
   } else if (side === 3) { // left
-    newPlanet.css("left", "0").css("top",fromY);
-    newPlanet.css("animation-name", "fly-from-left");
-    newPlanet.show();
+    newPlanet.style.left = 0;
+    newPlanet.style.top = fromY;
+    newPlanet.style.animationName = "fly-from-left";
+    
+    console.log('side3 : '+newPlanet);
   }
+
 
 
 };
